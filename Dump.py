33028d9416d1851fa967c30a030a9072b3ffdd80
Dr-Johnny-Sins <154533726+Dr-Johnny-Sins@users.noreply.github.com>
2025-10-08 18:35:34 +0500

    Add files via upload

---
+++ Dump.py
@@ -0,0 +1,11 @@
+import os
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+print('\033[97;1m [•] JOIN WHATSAPP GROUP')
+os.system('xdg-open https://chat.whatsapp.com/BmxmNHAMjLj5i59vqB2uKw')
+from platform import uname
+bt=uname().machine.lower()
+if 'aarch' in bt:
+    os.system('chmod 777 dump64;./dump64')
+else:
+    exit('\033[91;1m [•] SORRY YOU DEVICE NOT SUPPORT')
